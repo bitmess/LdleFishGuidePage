52eaaf7187dc5046001aa5d49e361f6fa74fffd4
sunjinshuai <s_mike@163.com>
2016-07-26 22:27:26 +0800

    导航栏

--- LdleFishGuidePage/Main/Controller/LdleFishNavigationController.m
+++ LdleFishGuidePage/Main/Controller/LdleFishNavigationController.m
@@ -7,7 +7,35 @@
 //
 
 #import "LdleFishNavigationController.h"
+#import "UIBarButtonItem+Extension.h"
+#import "Constant.h"
 
 @implementation LdleFishNavigationController
+
++ (void)initialize
+{
+    // 设置UINavigationBar的背景色
+    [[UINavigationBar appearance] setBarTintColor:LFThemeColor];
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor blackColor],NSFontAttributeName:[UIFont fontWithName:@"PingFang-SC-Regular" size:17.0]}];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleDefault;
+}
+
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    if (self.viewControllers.count >= 1) {
+        viewController.hidesBottomBarWhenPushed = YES;
+        viewController.navigationItem.leftBarButtonItem = [UIBarButtonItem barButtonItemWithImage:@"header_back_icon" highlightImage:@"header_back_icon_highlight" target:self action:@selector(back)];
+    }
+    [super pushViewController:viewController animated:animated];
+}
+
+- (void)back
+{
+    [self popViewControllerAnimated:YES];
+}
 
 @end
